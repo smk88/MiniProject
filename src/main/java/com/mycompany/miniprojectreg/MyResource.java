1c1a251474861cf02a509d852604dede6be6cb05
Saurabh <abc@gmail.com>
2015-09-21 12:05:50 +0530

    Added by Saurabh

---
+++ src/main/java/com/mycompany/miniprojectreg/MyResource.java
@@ -0,0 +1,82 @@
+package com.mycompany.miniprojectreg;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.MediaType;
+
+import java.sql.*;
+
+/**
+ * Example resource class hosted at the URI path "/myresource"
+ */
+
+@Path("/myresource")
+
+public class MyResource {
+
+    static String driver;
+    static String url;
+    static Connection con;
+    static Statement stm;
+    static ResultSet RS,RS1;
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/{myresource1}/{myresource2}")
+
+    public String getIt1(@PathParam("myresource1") String username, @PathParam("myresource2") String password) throws SQLException {
+        System.out.println(username + "\t" + password);
+        return connect(username, password);
+    }
+
+    public static String connect(String input1, String input2) throws SQLException {
+        String login = "false";
+        String str;
+        try {
+            
+            String url = "jdbc:mysql://localhost:3306/miniproject";
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection(url, "abc", "Project123");
+            str = "Database connection established";
+            System.out.println(str);
+
+            stm = con.createStatement();
+            System.out.println(input1 + "\t" + input2);
+            String sql = "SELECT * FROM login where username ='" + input1 + "' and Password ='" + input2 + "'";     // where Username = '"+str1+"' and Password = '"+str2+"' "; 
+            RS = stm.executeQuery(sql);
+
+            while (RS.next()) {
+                String Username = RS.getString("Username");
+                String Password = RS.getString("Password");
+                if (Username.equals(input1) & Password.equals(input2)) {
+
+                    System.out.println("Logged In Successfully !");
+                   String sql1 = "SELECT * FROM students2015_16 where GRNo='" + input1 + "'";
+                    RS1= stm.executeQuery(sql1);
+                    if(RS1.next())
+                    {
+                        System.out.println(RS1.getString("DivisionName") + RS1.getString("RollNo") + RS1.getString("GRNo") + RS1.getString("FirstName") + RS1.getString("MiddleName") + RS1.getString("SurName") + RS1.getString("Course") + RS1.getString("Branch") + RS1.getString("Class") + RS1.getString("ModuleName") + RS1.getString("Gender"));
+                        login= RS1.getString("DivisionName") +'-'+ RS1.getString("RollNo")+'-'+ RS1.getString("GRNo") +'-'+ RS1.getString("FirstName") + '-'+RS1.getString("MiddleName") + '-'+RS1.getString("SurName") +'-'+ RS1.getString("Course") +'-'+ RS1.getString("Branch") +'-'+ RS1.getString("Class") +'-'+ RS1.getString("ModuleName") +'-'+ RS1.getString("Gender");
+                    }
+                    return login;
+                } else {
+                    System.out.println("Invalid Username Or Password!");
+                    login = "false";
+                }
+            }
+        } catch (Exception err) {
+            System.out.println("ERROR: " + err);
+        }
+        return login;
+    }
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/{a}")
+        public String print()
+        {
+            return "Insufficient Data";
+        }
+
+}
---
+++ src/main/java/com/mycompany/miniprojectreg/MyResource1.java
@@ -0,0 +1,66 @@
+package com.mycompany.miniprojectreg;
+
+import java.sql.*;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import org.codehaus.jettison.json.JSONArray;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+
+@Path("/mydata")
+
+public class MyResource1 {
+
+    static String driver;
+    static String url;
+    static Connection con;
+    static Statement stm;
+    static ResultSet RS;
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/{myresource1}/{myresource2}")
+
+    public String getIt1(@PathParam("myresource1") String username, @PathParam("myresource2") String password) throws SQLException, JSONException {
+        System.out.println(username + "\t" + password);
+        return getJSON(username, password);
+    }
+
+
+    public static String getJSON(String in1, String in2) throws JSONException {
+        JSONArray array = new JSONArray();
+        try {
+
+            url = "jdbc:mysql://localhost:3306/miniproject";
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection(url, "abc", "Project123");
+            String str = "Database connection established";
+            System.out.println(str);
+
+            stm = con.createStatement();
+            System.out.println(in1 + "\t" + in2);
+            String sql = "SELECT * FROM students2015_16 where DivisionName ='" + in1 + "' and Class ='" + in2 + "'";     // where Username = '"+str1+"' and Password = '"+str2+"' "; 
+            RS = stm.executeQuery(sql);
+
+            while (RS.next()) {
+                String Name = RS.getString("FirstName") + " " + RS.getString("MiddleName") + " " + RS.getString("SurName");
+                String GrNo = RS.getString("GrNo");
+                String Rollno = RS.getString("RollNo");
+                JSONObject j = new JSONObject();
+                j.put("name", Name);
+                j.put("roll", Rollno);
+                j.put("grno", GrNo);
+                array.put(j);
+             }
+            System.out.println(array);
+            return array.toString();
+        } catch (Exception err) {
+            System.out.println("ERROR: " + err);
+        }
+        return null;
+
+    }
+}
